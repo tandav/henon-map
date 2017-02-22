d578393fe5be1a049711a4ff4e0a87099e99170b
tandav <alx1dr@gmail.com>
2017-02-22 09:44:11 +0300

    svg flexbox mvp

--- script.js
+++ script.js
@@ -1,141 +1,18 @@
-var n = 500; // number of points
-var r = 5.5;
 
-var a_min = -1.5;
-var a_max = 2.5;
-var b_min = -0.5;
-var b_max = 1.2;
+var n_plots = d3.select(".n_plots")
 
-document.querySelector('#a_slider').min = a_min;
-document.querySelector('#a_slider').max = a_max;
-document.querySelector('#b_slider').min = b_min;
-document.querySelector('#b_slider').max = b_max;
-document.querySelector('#a_slider').value = (a_min + a_max) / 2;
-document.querySelector('#b_slider').value = (b_min + b_max) / 2;
+n_plots.append("svg")
+	.attr("width", "25vw")
+	.attr("height", "12.5vw")
+	.attr("class", "xn_plot")
 
-var inputs = document.querySelectorAll('input');
-[].forEach.call(inputs, function(inp) {
-  inp.step = 0.001;
-});
+n_plots.append("svg")
+	.attr("width", "25vw")
+	.attr("height", "12.5vw")
+	.attr("class", "yn_plot")
 
-
-
-var width = 500;
-var height = 500;
-
-var xn_plot = d3.select('body')
-	.append('svg:svg')
-	.attr('width', width)
-	.attr('height', height)
-	.attr('class', 'xn_plot')
-
-var xy_plot = d3.select('xn-plot')
-	.append('svg')
-	// .attr('width', width + margin.right + margin.left)
-	// .attr('height', height + margin.top + margin.bottom)
-	.attr('class', 'xy_plot')
-	.on('mousemove', mouse_mooved)
-	// .attr("onmousemove", "mouse_mooved(event)")
-
-// var main = xy_plot.append('g')
-// 	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-// 	.attr('width', width)
-// 	.attr('height', height)
-// 	.attr('class', 'main')
-
-// x and y scales, I've used linear here but there are other options
-// the scales translate data values to pixel values for you
-// scales are constand (TODO zoom-sliders)
-var x = d3.scaleLinear()
-	.domain([-r, r]) // the range of the values to plot
-	.range([ 0, width ]); // the pixel range of the x-axis
-	// .domain([0, d3.max(xdata)]) // the range of the values to plot
-	// .range([ 0, width ]); // the pixel range of the x-axis
-var y = d3.scaleLinear()
-	.domain([-r, r])
-	.range([ height, 0 ]);
-
-var a_scale = d3.scaleLinear()
-	.domain([a_min, a_max])
-	.range([0, width])
-
-var b_scale = d3.scaleLinear()
-	.domain([b_min, b_max])
-	.range([height, 0])
-
-// draw the x axis
-var xAxis = d3.axisBottom(x)
-xy_plot.append('g')
-	.attr('transform', 'translate(0,' + height + ')')
-	.attr('class', 'main axis date')
-	.call(xAxis);
-// draw the y axis
-var yAxis = d3.axisLeft(y)
-	xy_plot.append('g')
-	// .attr('transform', 'translate(0,0)')
-	.attr('transform', 'translate(0,' + width + ')')
-	.attr('class', 'main axis date')
-	.call(yAxis);
-
-
-// // draw the x axis
-// var xAxis = d3.axisBottom(x)
-// main.append('g')
-// 	.attr('transform', 'translate(0,' + height + ')')
-// 	.attr('class', 'main axis date')
-// 	.call(xAxis);
-// // draw the y axis
-// var yAxis = d3.axisLeft(y)
-// 	main.append('g')
-// 	.attr('transform', 'translate(0,0)')
-// 	.attr('class', 'main axis date')
-// 	.call(yAxis);
-
-// draw the graph object
-var g = xy_plot.append("svg:g"); 
-
-function redraw() 
-{
-// 	g.selectAll("scatter-dots")
-// 		// .exit().remove()
-// 		.data(yy, function(d) { return d; });
-	
-	var xdata = [0.1];
-	var ydata = [0.2];
-	var a = document.getElementById("a_slider").value;
-	var b = document.getElementById("b_slider").value;
-
-
-	// Henon Map
-	for (var i = 0; i < n; i++) 
-	{
-		var x_next = 1 - a * Math.pow(xdata[xdata.length - 1], 2) + ydata[ydata.length - 1];
-		var y_next = b * xdata[xdata.length - 1];
-		xdata.push(x_next);
-		ydata.push(y_next);
-	}
-
-	g.selectAll("*").remove(); //without that line old data remains
-	g.selectAll("scatter-dots")
-		.data(ydata)  // using the values in the ydata array
-		.enter().append("svg:circle")  // create a new circle for each value
-		.attr("cy", function (d) { return y(d); } ) // translate y value to a pixel
-		.attr("cx", function (d,i) { return x(xdata[i]); } ) // translate x value
-		.attr("r", 1) // radius of circle
-		.style("opacity", 1.0); // opacity of circle
-		document.querySelector('#a_out').value = a;
-		document.querySelector('#b_out').value = b;
-}
-
-function mouse_mooved() 
-{
-	// var mouse_xy = [0, 0];
-	var mouse_xy = d3.mouse(this);
-	document.querySelector('#a_slider').value = a_scale.invert(mouse_xy[0]);
-	document.querySelector('#b_slider').value = a_scale.invert(mouse_xy[1]);
-	document.querySelector('#a_out').value = b_scale.invert(mouse_xy[0]);
-	document.querySelector('#b_out').value = b_scale.invert(mouse_xy[1]);
-	redraw();
-
-}
-// redraw(initial);
+var xy_plot = d3.select(".charts")
+	.append("svg")
+	.attr("width", "25vw")
+	.attr("height", "25vw")
+	.attr("class", "xy_plot")
