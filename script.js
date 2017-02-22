5c0d1ef4b3167af63e185b33230e2238f8782954
tandav <alx1dr@gmail.com>
2017-02-22 10:13:47 +0300

    make charts bigger

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-
-var n_plots = d3.select(".n_plots")
+var small_chart_height = "20vw";
+var all_charts_width = "41vw"; // small_chart_height*2 + 1vw_margin
+var n_plots = d3.select(".n_plots");
 
 n_plots.append("svg")
-	.attr("width", "31vw")
-	.attr("height", "15vw")
+	.attr("width", all_charts_width)
+	.attr("height", small_chart_height)
 	.attr("class", "xn_plot")
 
 n_plots.append("svg")
-	.attr("width", "31vw")
-	.attr("height", "15vw")
+	.attr("width", all_charts_width)
+	.attr("height", small_chart_height)
 	.attr("class", "yn_plot")
 
 var xy_plot = d3.select(".charts")
 	.append("svg")
-	.attr("width", "31vw")
-	.attr("height", "31vw")
+	.attr("width", all_charts_width)
+	.attr("height", all_charts_width)
 	.attr("class", "xy_plot")
