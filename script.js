3f00f04de8a6f856c8560d5f46c76cf9c68d8784
tandav <alx1dr@gmail.com>
2017-02-22 10:03:12 +0300

    fix margins

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 var n_plots = d3.select(".n_plots")
 
 n_plots.append("svg")
-	.attr("width", "25vw")
-	.attr("height", "12.5vw")
+	.attr("width", "31vw")
+	.attr("height", "15vw")
 	.attr("class", "xn_plot")
 
 n_plots.append("svg")
-	.attr("width", "25vw")
-	.attr("height", "12.5vw")
+	.attr("width", "31vw")
+	.attr("height", "15vw")
 	.attr("class", "yn_plot")
 
 var xy_plot = d3.select(".charts")
 	.append("svg")
-	.attr("width", "25vw")
-	.attr("height", "25vw")
+	.attr("width", "31vw")
+	.attr("height", "31vw")
 	.attr("class", "xy_plot")
