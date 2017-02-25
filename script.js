dfaf23d24441e64352a6cd2fc00e9971da49bb92
tandav <alx1dr@gmail.com>
2017-02-25 12:37:33 +0300

    add scatter plot
    - probably xn and yn plots works with bugs (they seems equal). Add this
    bug to TODO

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var n = 100;
+var n = 200;
 var small_chart_height = 250;
 var margin = 10;
 var all_charts_width = small_chart_height*2 + margin;
 // var all_charts_width = "41vw"; // small_chart_height*2 + 1vw_margin
 // var n_plots = d3.select(".n_plots");
-var radius = 5.5;
+var radius = 30; // x and y axes scope
 xn_plot = d3.select(".n_plots").append("svg")
 	.attr("width", all_charts_width)
 	.attr("height", small_chart_height)
@@ -94,3 +94,50 @@ var xy_yAxis = d3.axisLeft(xy_plot_yScale)
 xy_plot.append("g")
 	.attr("transform", "translate(" + (all_charts_width/2) + ",0)")
 	.call(xy_yAxis);
+
+var xn_dots = xn_plot.append("g");
+var yn_dots = yn_plot.append("g");
+var xy_dots = xy_plot.append("g");
+
+function redraw() {
+	var xdata = [0.1];
+	var ydata = [0.2];
+	var a = document.getElementById("a_slider").value;
+	var b = document.getElementById("b_slider").value;
+
+	// Henon Map
+	for (var i = 0; i < n; i++)
+	{
+		var x_next = 1 - a * Math.pow(xdata[xdata.length - 1], 2) + ydata[ydata.length - 1];
+		var y_next = b * xdata[xdata.length - 1];
+		xdata.push(x_next);
+		ydata.push(y_next);
+	}
+
+	xn_dots.selectAll("*").remove(); //without that line old data remains
+	xn_dots.selectAll("scatter-dots")
+		.data(xdata)  // using the values in the xdata array
+		.enter().append("svg:circle")  // create a new circle for each value
+		.attr("cx", function (d,i) { return xn_plot_xScale(i); } ) // translate x value
+		.attr("cy", function (d) { return xn_plot_yScale(d); } ) // translate y value to a pixel
+		.attr("r", 1) // radius of circle
+		.style("opacity", 1.0); // opacity of circle
+
+	yn_dots.selectAll("*").remove(); //without that line old data remains
+	yn_dots.selectAll("scatter-dots")
+		.data(ydata)  // using the values in the ydata array
+		.enter().append("svg:circle")  // create a new circle for each value
+		.attr("cx", function (d,i) { return xn_plot_xScale(i); } ) // translate x value
+		.attr("cy", function (d) { return xn_plot_yScale(d); } ) // translate y value to a pixel
+		.attr("r", 1) // radius of circle
+		.style("opacity", 1.0); // opacity of circle
+
+	xy_dots.selectAll("*").remove(); //without that line old data remains
+	xy_dots.selectAll("scatter-dots")
+	.data(ydata)  // using the values in the ydata array
+		.enter().append("svg:circle")  // create a new circle for each value
+		.attr("cx", function (d,i) { return xy_plot_xScale(xdata[i]); } ) // translate x value
+		.attr("cy", function (d) { return xy_plot_yScale(d); } ) // translate y value to a pixel
+		.attr("r", 1) // radius of circle
+		.style("opacity", 1.0); // opacity of circle
+}
